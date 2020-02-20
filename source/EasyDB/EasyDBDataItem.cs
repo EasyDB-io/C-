6eccf3df2f38b9ce285f21c2135c9880c125108a
Brett <brett.beard@gtri.gatech.edu>
2020-02-20 13:59:04 -0500

    Implemented EasyDB interface for C#.

---
+++ source/EasyDB/EasyDBDataItem.cs
@@ -0,0 +1,49 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EasyDBDataItem.cs" company="The Public">
+// Copyright 2020 The Public
+//
+// EasyDB C# is free software: you can redistribute it and/or modify 
+// it under the terms of the GNU General Public License as published by the 
+// Free Software Foundation, either version 3 of the License, or (at your 
+// option) any later version.
+//
+// EasyDB C# is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General 
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License 
+// along with EasyDB c#.  If not, see http://www.gnu.org/licenses/.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace EasyDB
+{
+    using System;
+
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// EasyDB data item class.
+    /// </summary>
+    public class EasyDBDataItem
+    {
+        /// <summary>
+        /// Gets or sets the key.
+        /// </summary>
+        /// <value>
+        /// The key.
+        /// </value>
+        [JsonProperty(PropertyName = "key")]
+        public String Key { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
+        [JsonProperty(PropertyName = "value")]
+        public String Value { get; set; }
+    }
+}
